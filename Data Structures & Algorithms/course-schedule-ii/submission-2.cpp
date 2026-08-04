c98a996f4163dbad1d6c9c2540910780f7542c2b
Deepak S <deepusuresh1212@gmail.com>
2026-08-04 23:26:51 +0530

    Add: course-schedule-ii - submission-2

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-2.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    // Topo sort
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        vector<int> indegree(numCourses, 0);
+
+        //Build the graph
+        for(auto& p: prerequisites){
+            adj[p[1]].push_back(p[0]);
+            indegree[p[0]]++;
+        }
+
+        queue<int> q;
+
+        // Push all nodes having indegree 0
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+
+        vector<int> ans;
+
+        while(!q.empty()){
+            auto node = q.front();
+            q.pop();
+
+            ans.push_back(node);
+
+            for(auto& n: adj[node]){
+                indegree[n]--; // marking the prerequisite course as done
+
+                if(indegree[n] == 0){
+                    q.push(n);
+                }
+            }
+        }
+
+        if(ans.size() != numCourses){ // Not all courses have been completed
+            return {};
+        }
+
+        return ans;
+    }
+};
